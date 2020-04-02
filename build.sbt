867b204a4fa7c0e7163617f587976de458ac4642
shenryhen <henry.shen@outlook.com>
2020-04-01 20:56:29 -0400

    can update address now

--- build.sbt
+++ build.sbt
@@ -4,4 +4,4 @@ organization := "ch.epfl.scala"
 version := "1.0"
 
 libraryDependencies +=  "org.scalaj" %% "scalaj-http" % "2.4.2"
-
+libraryDependencies += "io.circe" %% "circe-parser" % "0.12.3"
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,10 +1,39 @@
 import scalaj.http._
+import cats.syntax.either._
+import io.circe._, io.circe.parser._
+
+// class Account {
+// 	val accounID: String
+// 	val firstName: String
+// 	val lastName: String
+// }
 
 object Main extends App {
+	// var is mutable
+	// val is immutable
     val KEY: String = sys.env.getOrElse("NESSIE_API_KEY", "")
     println(KEY)
     val BANK_URL: String = "http://api.reimaginebanking.com"
-    val response: HttpResponse[String] = Http(BANK_URL + "/atms").param("key", KEY).asString
-    println(response.body)
-    println(response.code)
+    var response: HttpResponse[String] = Http(BANK_URL + "/atms").param("key", KEY).asString
+    //print()
+    val customerID = "5d6ecf67322fa016762f2f6d"
+    val customerURL = "/customers/" + customerID
+    response = Http(BANK_URL+customerURL).param("key", KEY).asString
+    val customer: String = response.body
+    println(customer)
+    var doc: Json = parse(customer).getOrElse(Json.Null)
+    val cursor: HCursor = doc.hcursor
+    //val firstName: Decoder.Result[String] = cursor.downField("first_name").as[String].toString()
+    var firstName: String = cursor.downField("first_name").as[String].productElement(0).toString()
+    println(s"Customer first name $firstName")
+    val modCursor: ACursor = cursor.downField("first_name").withFocus(_.mapString(s => "Raquel"))
+    println(modCursor.top)
+    val modifiedField: String = """{"street_name": "Octocat St", "city": "Atlanta", "state": "GA"}"""
+    println(BANK_URL+customerURL)
+    // val putResponse: HttpResponse[String] = Http(BANK_URL+customerURL).param("key", KEY).method("PUT").postData(modifiedField).asString
+    // println(putResponse.body)
+    // println(putResponse.code)
+   	// println(putResponse.headers)
+
+   	//POST for /account, /bill
 }
