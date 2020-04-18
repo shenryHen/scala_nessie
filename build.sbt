3fc800f47b6bdb08cb90077d1f75414e4c6bb372
mlh-henry <henry.shen@outlook.com>
2020-04-17 21:06:33 -0400

    new library:

--- build.sbt
+++ build.sbt
@@ -1,7 +1,8 @@
 scalaVersion := "2.13.1"
 name := "scala-nessie"
-organization := "ch.epfl.scala"
+organization := "cof"
 version := "1.0"
 
-libraryDependencies +=  "org.scalaj" %% "scalaj-http" % "2.4.2"
-
+//libraryDependencies +=  "org.scalaj" %% "scalaj-http" % "2.4.2"
+libraryDependencies += "com.typesafe.akka" %% "akka-http"   % "10.1.11" 
+libraryDependencies += "com.typesafe.akka" %% "akka-stream" % "2.5.26" // or whatever the latest version is
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,10 +1,31 @@
-import scalaj.http._
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model._
+import akka.stream.ActorMaterializer
+import akka.http.scaladsl.model.HttpRequest
+import akka.http.scaladsl.unmarshalling.Unmarshal
+
+
+import scala.concurrent.Future
+import scala.util.{ Failure, Success }
 
 object Main extends App {
     val KEY: String = sys.env.getOrElse("NESSIE_API_KEY", "")
-    println(KEY)
     val BANK_URL: String = "http://api.reimaginebanking.com"
-    val response: HttpResponse[String] = Http(BANK_URL + "/atms").param("key", KEY).asString
-    println(response.body)
-    println(response.code)
+   	implicit val system = ActorSystem()
+    implicit val materializer = ActorMaterializer()
+    // needed for the future flatMap/onComplete in the end
+    implicit val executionContext = system.dispatcher
+ 	val req = HttpRequest(
+		method = HttpMethods.GET,
+		uri = BANK_URL+"/atms/?key=" + KEY,
+		entity = HttpEntity(ContentTypes.`text/plain(UTF-8)`, "data")
+	)
+	val responseFuture: Future[HttpResponse] = Http().singleRequest(req)
+	responseFuture
+      .onComplete {
+        case Success(res) => println(Unmarshal(res).to[String])
+
+        case Failure(_)   => sys.error("something wrong")
+      }
 }
